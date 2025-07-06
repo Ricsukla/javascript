6d6753b894d767000bbdf12e1ee7197c94f240c7
Ricsukla <ricsukla@gmail.com>
2025-07-06 17:25:00 +0000

    array

---
+++ 2.basics/1.js
@@ -0,0 +1,15 @@
+const myArr=[2,3,1,1];
+const myArr2=new Array(45,67,33)
+console.log(myArr[0])
+//methods
+myArr.push(9)
+myArr.pop()
+myArr.unshift(6)//adds in front
+//myArr.shift() removes front wla
+console.log(myArr)
+const newArr= myArr.join()
+console.log(myArr.includes(9));
+console.log(myArr.indexOf(36));
+console.log(myArr);
+console.log(newArr);
+console.log(typeof newArr);
