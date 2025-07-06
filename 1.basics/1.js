cdae25e4b5a5dbdd119a9898243ecd24c04c0196
Ricsukla <ricsukla@gmail.com>
2025-07-06 04:40:35 +0000

    variables

--- 1.basics/1.js
+++ 1.basics/1.js
@@ -1,1 +1,10 @@
-console.log("bittu")
+const accountId=14455
+let accountEmail="richa@google.com"
+var accountPassword="3355"
+let accountState;
+accountCity="Delhi"//without let and var printed, possible not recommended
+//accountId=3 const cant be changed
+accountCity="Bhubaneswar"//see changed
+console.log(accountId)
+//printing one by one is tiring
+console.table([accountCity,accountEmail,accountPassword,accountId,accountState])
---
+++ 1.basics/2.js
@@ -0,0 +1,5 @@
+"use strict";//treats all JS code as newer version,even though default chlta newer version pr
+//alert(3+3) this will give an error as we are working on node.js, browser ke right click ke baad inspect, fir console mein chl jaega , 6 will come as an output in popup
+console.log(typeof "richa")
+console.log(typeof null)
+console.log(typeof undefined)
