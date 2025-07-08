7fc8059828bd5df61d118792dcd5cbffb94a26b1
Ricsukla <ricsukla@gmail.com>
2025-07-08 03:02:19 +0000

    while

---
+++ 3.basics/3.js
@@ -0,0 +1,20 @@
+let index=0
+while(index<=8){
+    console.log(`Value of index is ${index}`);
+    index++
+    
+}
+let myArr=['richa','bitu','richard']
+let arr=0
+while (arr<myArr.length){
+    console.log(`the element value are ${myArr[arr]}`);
+    arr++;
+    
+}
+//do-while
+let score=1
+do{
+    console.log(`Score is ${score}`);
+    score++
+    
+}while(score<=4)
